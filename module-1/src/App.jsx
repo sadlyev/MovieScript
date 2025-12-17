45e358076cac88870bce023004318f4dfc3e0b64
Asilbek Sadiyev <201261859+dilo2222@users.noreply.github.com>
2025-12-17 23:56:11 +0500

    changed all

--- module-1/src/App.jsx
+++ module-1/src/App.jsx
@@ -1,23 +1,38 @@
-import { useState } from 'react'
-import './App.css'
-import {MovieList} from "./MovieList"
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [titleText,  setTitleText] = useState("")
+  const [titleLists, setTitleList] = useState([])
 
-  const [showText, setShowText] = useState(false)
-
-  const handleBtn = () => {
-    setShowText(true)
+  const handleText = (e) => {
+    setTitleText(e.target.value)
   }
 
+  const handleList = (e) => {
+    e.preventDefault();
+     setTitleList(prevTitleLists => [...prevTitleLists, titleText])
+     setTitleText(" ")
+  }
+
+  console.log(titleLists)
   return (
     <div>
-      <button onClick={handleBtn}>Add a Movie</button>
+      <form onSubmit={handleList}>
+        <label>
+          <input placeholder="Title" value={titleText} onChange={handleText}></input>
+        </label>
+        <button type="submit">Add Element</button>
+      </form>
+      <ul>
 
-      {showText && <MovieList/>}
-      
+        {titleLists.map((item, index) => item.length > 1 ? <li key={index}>{item}</li> : null) }
+
+
+
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- module-1/src/Movie.jsx
+++
@@ -1,8 +0,0 @@
-export function Movie(list) {
-
-    return (
-        <ul>
-            {list.map((item) => <li><span>{item.title}</span><span>{item.photo}</span></li>)}
-        </ul>
-    )
-}
