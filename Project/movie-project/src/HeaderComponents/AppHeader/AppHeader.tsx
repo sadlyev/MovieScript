ac243353edf8b782a27563a0265735b0de4c7a24
Asilbek Sadiyev <kvaradonauz@gmail.com>
2026-02-17 02:16:28 +0500

    top is visually ready

--- Project/movie-project/src/HeaderComponents/AppHeader/AppHeader.tsx
+++ Project/movie-project/src/HeaderComponents/AppHeader/AppHeader.tsx
@@ -1,19 +1,23 @@
 import { Link } from "react-router-dom"
-import AppLogo from "../../assets/Mask group.svg"
+import AppLogo from "../../assets/маруся white.svg"
+import SearchIcon from "../../assets/Vector.svg"
+import "./AppHeader.css"
 
 const AppHeader = () => {
     
     return (
-        <header>
-            <div>
-                <img src={AppLogo}></img>
-                <p>маруся</p>
-            </div>
-            <Link to="/">Главная</Link>
-            <Link to="">Жанры</Link>
-            <label>
-                <input></input>
+        <header className="header">
+                <Link  className="header_link" to="/"><img src={AppLogo} className="header_logo" width="143" height="32"></img></Link>
+            <div className="header_nav-wrapper">
+                 <Link className="header_link" to="/">Главная</Link>
+            <Link className="header_link" to="/genres">Жанры</Link>
+            <label className="header_label">
+                <img src={SearchIcon} className="header_label-logo" width="20" height="20"></img>
+                <input className="header_label-input" placeholder="поиск"></input>
             </label>
+            </div>
+           
+            <Link className="header_link" to="/profile">Войти</Link>
 
         </header>
     )
